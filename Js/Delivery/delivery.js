0e964eacb0c05781ae86a2a573f76057c1952df9
Gabriel Da fonseca <123200003+gabri05el@users.noreply.github.com>
2024-02-13 11:13:42 -0300

    Add files via upload

---
+++ Js/Delivery/delivery.js
@@ -0,0 +1,61 @@
+/*
+const campoAgua = document.querySelector(".delivery__campo_agua .delivery__campo");
+const campoCocaCola = document.querySelector(".delivery__campo_coca_cola");
+const campoFanta = document.querySelector(".delivery__campo_fanta");
+const campoGuarana = document.querySelector(".delivery__campo_guarana");
+const campoPepsi = document.querySelector(".delivery__campo_pepsi");
+const campoSprite = document.querySelector(".delivery__campo_sprite");
+const campoSucoNatural = document.querySelector(".delivery__campo_suco_natural");
+const campoVinho = document.querySelector(".delivery__campo_vinho");
+const campoPizza = document.querySelector(".delivery__campo_pizza");
+
+campoAgua.onclick = function () {
+    escolhido();
+}
+
+campoAgua.addEventListener("click", (evento) => {
+     evento.preventDefault();
+     escolhido();
+ })
+campoCocaCola.addEventListener("click", (evento) => {
+     evento.preventDefault();
+     escolhido();
+ })
+campoFanta.addEventListener("click", (evento) => {
+     evento.preventDefault();
+     escolhido();
+ })
+campoGuarana.addEventListener("click", (evento) => {
+     evento.preventDefault();
+     escolhido();
+ })
+campoPepsi.addEventListener("click", (evento) => {
+     evento.preventDefault();
+     escolhido();
+ })
+campoSprite.addEventListener("click", (evento) => {
+     evento.preventDefault();
+     escolhido();
+ })
+campoSucoNatural.addEventListener("click", (evento) => {
+     evento.preventDefault();
+     escolhido();
+ })
+campoVinho.addEventListener("click", (evento) => {
+     evento.preventDefault();
+     escolhido();
+ })
+campoPizza.addEventListener("click", (evento) => {
+    evento.preventDefault();
+    escolhido();
+ })
+
+function escolhido() {
+    var campo = document.querySelector(".delivery__campo");
+    var campoNome = document.querySelector(".delivery__campo_nome");
+    var campoValor = document.querySelector(".delivery__campo_valor");
+    campo.style.background = "#6C5F5B";
+    campoNome.style.color = "#F6F1EE";
+    campoValor.style.color = "#F6F1EE";
+}
+*/
